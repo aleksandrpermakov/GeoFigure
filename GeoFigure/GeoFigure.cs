c57db5f31f04ec0fe92192196197c713a2081cdb
Professional <Professional@USER-PC>
2025-04-21 17:26:08 +0500

    Добавьте файлы проекта.

---
+++ GeoFigure/GeoFigure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoFigure
+{
+    internal abstract class GeoFigure
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            //set { name = value; }
+        }
+        public GeoFigure(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+        public abstract float GetPerimetr();
+    }
+    public class FourAngle : GeoFigure
+    {
+        float a;
+        float b;
+        float c;
+        float d;
+        public FourAngle(string name, float a = 1, float b = 1,
+            float c = 1, float d = 1) : base(name)
+        {
+            this.a = a; this.b = b;
+            this.c = c; this.d = d;
+        }
+        public override float GetPerimetr()
+        {
+            return a + b + c + d;
+        }
+        public override string ToString()
+        {
+            return $"{Name} сторона a = {a}, сторона b = {b}, " +
+                $"сторона c = {c}, сторона d = {d}. Периметр = {this.GetPerimetr()}";
+        }
+    }
+}
+
---
+++ GeoFigure/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoFigure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FourAngle> fourAngles = new List<FourAngle>();
+            string filename = "Figures.txt";
+            string myDocs = FSWorker.myDocs;
+            for (int i = 0; i < 3; i++)
+            {
+                var number = new Random((int)DateTime.Now.Ticks);
+                var _figure = new FourAngle($"Фигура №{i + 1}",
+                    1 + number.Next(9),
+                    1 + number.Next(8) + (float)number.NextDouble(),
+                    1 + number.Next(9),
+                    1 + number.Next(9));
+                fourAngles.Add(_figure);
+                FSWorker.WriteStrToFile
+                    ($"{myDocs}\\{filename}", _figure.ToString());
+            }
+
+            //Console.WriteLine($"Мои документы - {FSWorker.myDocs}");
+            //foreach (string filename in FSWorker.ContentOfDir(FSWorker.myDocs))
+            //{
+            //    Console.WriteLine(filename);
+            //}
+            //Console.WriteLine($"Рабочий стол - {FSWorker.myDesktop}");
+            //foreach (string filename in FSWorker.ContentOfDir(FSWorker.myDesktop))
+            //{
+            //    Console.WriteLine(filename);
+            //}
+        }
+    }
+    }
+}
