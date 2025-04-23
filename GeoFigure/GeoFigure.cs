81ef94a7316ff4663a1aadbfea657f1ba5f0c0cb
aleksandrpermakov <aleksandrpermakov105@gmail.com>
2025-04-23 23:26:28 +0500

    добавил круг и сеттеры

--- GeoFigure/GeoFigure.cs
+++ GeoFigure/GeoFigure.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,16 +24,26 @@ namespace GeoFigure
         {
             return name;
         }
-        public abstract int GetPerimetr();
-        public abstract void OutPutFigureToFile();
+        public abstract double GetPerimetr();//периметр
+        // public abstract void OutPutFigureToFile();
+        public abstract double GetArea(); //площадь
     }
-    public class FourAngle : GeoFigure
+    public class FourAngle : GeoFigure //четырехугольник
     {
         int a;
         int b;
         int c;
         int d;
-        
+
+        public int GetA() { return a; }
+        public int GetB() { return b; }
+        public int GetC() { return c; }
+        public int GetD() { return d; }
+
+        public void SetA (int a) { this.a = a; }
+        public void SetB (int b ) { this.b = b; }
+        public void SetC(int c) { this.c = c; }
+        public void SetD(int d) { this.d = d; }
         public FourAngle(string name, int a = 1, int b = 1,
             int c = 1, int d = 1) : base(name)
         {
@@ -46,12 +57,12 @@ namespace GeoFigure
             {
                 this.a = a; this.b = b;
                 this.c = c; this.d = d;
-                
+
             }
-            
+
         }
-       
-        public override int GetPerimetr()
+
+        public override double GetPerimetr()
         {
             return a + b + c + d;
         }
@@ -60,11 +71,74 @@ namespace GeoFigure
             return $"{GetName} сторона a = {a}, сторона b = {b}, " +
                 $"сторона c = {c}, сторона d = {d}. Периметр = {this.GetPerimetr()}";
         }
-        public override void  OutPutFigureToFile()
+        public override double GetArea()
         {
-            this.ToString();//заглушка
+            Console.WriteLine("Плащадь верна, при условии , что это квадрат или прямоугольник!");
+            return this.a * this.b; 
+        }
+        //public override void  OutPutFigureToFile()
+        //{
+        //    string path = "Table.txt";
+        //    string content = "content";
+        //    StreamWriter sr = new StreamWriter(path, content, true);
+        //    sr.Write(content);
+        //    sr.Close();
+        //    this.ToString();//заглушка
+        //}
+
+    }
+    public class Triangle : GeoFigure //треугольник
+    {
+        int a;
+        int b;
+        int c;
+
+        public int GetA() { return a; }
+        public int GetB() { return b; }
+        public int GetC() { return c; }
+        public void SetA(int a) { this.a = a; }
+        public void SetB(int b) { this.b = b; }
+        public void SetC(int c) { this.c = c; }
+        public Triangle(string name, int a = 1, int b = 1,
+            int c = 1) : base(name)
+        {
+            this.a = a; this.b = b; this.c = c;
+        }
+        public override double GetPerimetr()
+        {
+            return a + b + c;
+        }
+        public override string ToString()
+        {
+            return $"{GetName} сторона a = {a}, сторона b = {b}, " +
+                $"сторона c = {c}. Периметр = {this.GetPerimetr()}";
+        }
+        public override double GetArea()
+        {
+            double area = 0;
+            double p_ = this.GetPerimetr() / 2; //полупериметр
+            area = Math.Sqrt((p_ * (p_ - this.a) * (p_ - this.b) * (p_ - this.c)));
+            return area;
         }
 
     }
+    public class Circle : GeoFigure
+    {
+        int d; //диаметр
+        public Circle(string name, int d = 1) : base(name)
+        { this.d = d; }
+
+        public int GetD() { return d; }
+        public void SetD(int d) { this.d = d; }
+        public override double GetPerimetr()
+        {
+
+            return this.d * Math.PI;
+        }
+        public override double GetArea()
+        { 
+            return  Math.PI * (this.d / 2) * (this.d / 2); //2Pr^2
+        }
+     }
 }
 
